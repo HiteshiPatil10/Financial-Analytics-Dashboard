b45b8413a93edddadd33944b5de28f0626e96dc3
HiteshiPatil10 <hphiteshipatil@gmail.com>
2025-07-01 13:24:04 +0530

    Update api.ts

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://finance-api-anns.onrender.com',
+  baseURL: 'https://finance-api-anns.onrender.com/api',
   withCredentials: false,
 });
 
